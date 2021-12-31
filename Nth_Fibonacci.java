accb5fd2d066abacba912416d7dc01fd54d94340
Toshiet Daksh <43781903+scythecarrier@users.noreply.github.com>
2021-12-31 15:19:15 +0530

    Add files via upload

---
+++ Nth_Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Nth_Fibonacci {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if(n==1){
+            System.out.println(n);
+        }
+        else{
+            int a = 1,b = 1;
+            int F = 0;
+            for(int i = 1; i<=n-2; i++){
+                F = a+b;
+                a = b;
+                b = F;
+            }
+            System.out.println(F);
+        }
+    }
+}
