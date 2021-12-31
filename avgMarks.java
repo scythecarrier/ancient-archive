5825d91b153d6f34b9b733985122ef2ec1113c71
Toshiet Daksh <43781903+scythecarrier@users.noreply.github.com>
2021-12-31 15:10:38 +0530

    Update avgMarks.java

--- avgMarks.java
+++ avgMarks.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class avgMarks {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		            Scanner s = new Scanner(System.in);
 		            System.out.println("Enter your name.");
 		            String name = s.next();
@@ -16,11 +15,5 @@ public class avgMarks {
 		            int avg = (m1 + m2 + m3)/3;
 		            System.out.println(ch);
 		            System.out.print(avg);
-				
-				/* Your class should be named Solution.
-			 	* Read input as specified in the question.
-			 	* Print output as specified in the question.
-				*/
-
 	}
 }
